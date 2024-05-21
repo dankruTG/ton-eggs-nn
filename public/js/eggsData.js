99c1c01ade9ee36912bbd641f9df3b32b798e55d
dankruTG <dankrute@gmail.com>
2024-05-21 19:46:57 +0300

    Изменения стиля

--- public/js/eggsData.js
+++ public/js/eggsData.js
@@ -1,8 +1,8 @@
 const eggs = [
-    { name: "Rubin Egg", icon: "public/images/rareEggs/RubinEgg.png", rarity: "Rare", strength: 10, minting: "no" },
-    { name: "Golden Egg", icon: "public/images/uncommonEggs/goldenEgg.png", rarity: "Uncommon", strength: 10, minting: "no" },
-    { name: "Silver Egg", icon: "public/images/uncommonEggs/silverEgg.png", rarity: "Uncommon", strength: 10, minting: "no" },
-    { name: "Bronze Egg", icon: "public/images/commonEggs/bronzeEgg.png", rarity: "Common", strength: 10, minting: "no" }
+    { name: "Rubin Egg", icon: "public/images/rareEggs/RubinEgg.png", rarity: "Rare", strength: 10000, minting: "no" },
+    { name: "Golden Egg", icon: "public/images/uncommonEggs/goldenEgg.png", rarity: "Uncommon", strength: 1000, minting: "no" },
+    { name: "Silver Egg", icon: "public/images/uncommonEggs/silverEgg.png", rarity: "Uncommon", strength: 1000, minting: "no" },
+    { name: "Bronze Egg", icon: "public/images/commonEggs/bronzeEgg.png", rarity: "Common", strength: 100, minting: "no" }
     // Добавьте сколько угодно объектов с данными о яйцах
 ];
 function findEggByName(eggName) {
