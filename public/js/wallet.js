011db464fd46d0289fb9b9b140f1e17d40225e14
dankruTG <dankrute@gmail.com>
2024-05-21 22:17:22 +0300

    add ton-connect

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -9,18 +9,29 @@ const walletStatus = document.getElementById('walletTaskStatus');
 
 connectWalletButton.addEventListener('click', async () => {
     try {
+        // Получаем список доступных кошельков
         const walletsList = await connector.getWallets();
         console.log(walletsList);
 
-        // Предположим, что пользователь выбрал первый кошелек из списка
-        const selectedWallet = walletsList[0];
+        // Проверяем, что есть доступные кошельки
+        if (walletsList.length > 0) {
+            // Предположим, что пользователь выбрал первый кошелек из списка
+            const selectedWallet = walletsList[0];
 
-        // Подключение к выбранному кошельку
-        await connector.connect(selectedWallet);
-        
-        const account = connector.account;
-        walletStatus.textContent = 'Done!';
-        console.log('Wallet address:', account.address);
+            // Подключение к выбранному кошельку
+            const connectionSource = {
+                universalLink: selectedWallet.universalLink,
+                bridgeUrl: selectedWallet.bridgeUrl
+            };
+            await connector.connect(connectionSource);
+
+            // Обновляем статус и выводим информацию о кошельке
+            const account = connector.account;
+            walletStatus.textContent = 'Done!';
+            console.log('Wallet address:', account.address);
+        } else {
+            walletStatus.textContent = 'No wallets found';
+        }
     } catch (error) {
         console.error('Error connecting wallet:', error);
         walletStatus.textContent = 'none';
