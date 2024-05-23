2dffdb233d5f3ec5b5d1c7099102f7d6ed3ad9b6
dankruTG <dankrute@gmail.com>
2024-05-23 19:27:15 +0300

    add ton-connect

--- public/js/TonWallet.js
+++ public/js/TonWallet.js
@@ -1,40 +1,6 @@
-import { TonConnect } from '@tonconnect/sdk';
+src="https://unpkg.com/@tonconnect/ui@0.0.9/dist/tonconnect-ui.min.js"
 
-const connector = new TonConnect({
-  manifestUrl: 'https://github.com/dankruTG/ton-eggs-nn/blob/master/tonconnect-manifest.json'
-});
-
-
-const connectWalletButton = document.getElementById('connectWalletButton');
-const walletStatus = document.getElementById('walletTaskStatus');
-
-connectWalletButton.addEventListener('click', async () => {
-    try {
-        // Получаем список доступных кошельков
-        const walletsList = await connector.getWallets();
-        console.log(walletsList);
-
-        // Проверяем, что есть доступные кошельки
-        if (walletsList.length > 0) {
-            // Предположим, что пользователь выбрал первый кошелек из списка
-            const selectedWallet = walletsList[0];
-
-            // Подключение к выбранному кошельку
-            const connectionSource = {
-                universalLink: selectedWallet.universalLink,
-                bridgeUrl: selectedWallet.bridgeUrl
-            };
-            await connector.connect(connectionSource);
-
-            // Обновляем статус и выводим информацию о кошельке
-            const account = connector.account;
-            walletStatus.textContent = 'Done!';
-            console.log('Wallet address:', account.address);
-        } else {
-            walletStatus.textContent = 'No wallets found';
-        }
-    } catch (error) {
-        console.error('Error connecting wallet:', error);
-        walletStatus.textContent = 'none';
-    }
+const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+    manifestUrl: 'https://github.com/dankruTG/ton-eggs-nn/blob/master/tonconnect-manifest.json',
+    buttonRootId: 'connectWalletButton'
 });
