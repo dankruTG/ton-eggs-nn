0ff1333accf3a8c61bb178cbc5cdee2c194bdc75
dankruTG <dankrute@gmail.com>
2024-05-24 14:08:04 +0300

    bot-tg integration1

--- public/js/TonWallet.js
+++
@@ -1,7 +0,0 @@
-import { TonConnectUI } from '@tonconnect/ui';
-
-
-const tonConnectUI = new TonConnectUI({
-    manifestUrl: 'https://github.com/dankruTG/ton-eggs-nn/blob/master/tonconnect-manifest.json',
-    buttonRootId: 'walletContainer'
-});
