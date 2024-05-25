8b4ecaf9388c0647b91737b443c6deae252dc5f8
dankruTG <dankrute@gmail.com>
2024-05-25 22:45:45 +0300

    addEggs 0.8

--- public/js/addEggs.js
+++ public/js/addEggs.js
@@ -35,7 +35,7 @@ function restoreInventory() {
 }
 
 // Функция для добавления яйца в инвентарь
-export async function addEggToInventory(egg) {
+export function addEggToInventory(egg) {
     const inventoryContainer = document.getElementById('inventoryItemsContainer');
     if (inventoryContainer) {
         const eggContainer = createEggContainer(egg);
