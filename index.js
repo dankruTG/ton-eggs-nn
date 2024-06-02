4154066a620444028af894e863c5c04fdff6c615
dankruTG <dankrute@gmail.com>
2024-06-02 23:11:37 +0300

    Добавление .env файла и обновление .gitignore для игнорирования .env

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
+
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// Устанавливаем статическую папку
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('index', {
-    firebaseConfig: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID
-    }
-  });
+    res.sendFile(path.join(__dirname, 'views', 'index.ejs'));
 });
 
-// Запуск сервера
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.get('/firebaseConfig', (req, res) => {
+    res.json({
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
