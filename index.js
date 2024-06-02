5e94e8fc423b174bc9b9915cdac322f8302c13fb
dankruTG <dankrute@gmail.com>
2024-06-02 22:23:42 +0300

    Добавление .env файла и обновление .gitignore для игнорирования .env

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+
+const app = express();
 const port = 3000;
 
-// Устанавливаем статическую папку
-app.use(express.static(path.join(__dirname)));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-// Отправка index.html при запросе на корень
+// Устанавливаем статическую папку
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.render('index', {
+    firebaseConfig: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID
+    }
+  });
 });
 
 // Запуск сервера
--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,21 +1,12 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
 import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
-};
+const firebaseConfig = JSON.parse(document.getElementById('firebaseConfig').textContent);
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Function to save progress
 export async function saveProgress(userId, data) {
     const docRef = doc(db, "users", userId.toString());
     const docSnap = await getDoc(docRef);
@@ -33,7 +24,6 @@ export async function saveProgress(userId, data) {
     console.log('Progress saved');
 }
 
-// Function to get progress
 export async function getProgress(userId) {
     const docRef = doc(db, "users", userId.toString());
     const docSnap = await getDoc(docRef);
@@ -41,7 +31,6 @@ export async function getProgress(userId) {
         return docSnap.data();
     } else {
         console.log('No such document! Creating new user...');
-        // Create a new user with initial data
         const initialData = {
             userId: userId.toString(),
             username: '',
