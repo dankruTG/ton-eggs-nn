9e7af3e241a7740f2405eb81e61a3563cd173f61
dankruTG <dankrute@gmail.com>
2024-06-02 20:33:06 +0300

    Добавление .env файла и обновление .gitignore для игнорирования .env

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,14 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
 import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBtaQXXyMt2Cf5EJVbR-TW4lztiyvdIcgI",
-    authDomain: "ton-eggs.firebaseapp.com",
-    projectId: "ton-eggs",
-    storageBucket: "ton-eggs.appspot.com",
-    messagingSenderId: "259366486455",
-    appId: "1:259366486455:web:1b346a97bb31b3ef196832"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
