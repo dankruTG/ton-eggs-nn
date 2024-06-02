43e4907a87451c742d3d6e86f8e3214afc21479b
dankruTG <dankrute@gmail.com>
2024-06-02 23:44:56 +0300

    back2

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-require('dotenv').config();
 
 // Устанавливаем статическую папку
 app.use(express.static(path.join(__dirname)));
