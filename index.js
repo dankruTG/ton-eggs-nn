525d9214edd3dd97699dd08d8f88b4df2d31065f
dankruTG <dankrute@gmail.com>
2024-06-02 23:13:38 +0300

    Добавление .env файла и обновление .gitignore для игнорирования .env

--- index.js
+++ index.js
@@ -8,22 +8,23 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/firebaseConfig', (req, res) => {
+  res.json({
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  });
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.ejs'));
 });
 
-app.get('/firebaseConfig', (req, res) => {
-    res.json({
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-    });
-});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
