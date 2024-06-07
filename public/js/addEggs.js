706135e385d55d2afd8825b2bc13e8bb9644d9f6
dankruTG <dankrute@gmail.com>
2024-06-07 18:54:46 +0300

    tw2back+finichfix

--- public/js/addEggs.js
+++ public/js/addEggs.js
@@ -413,7 +413,7 @@ async function finishDiggEgg(rarity) {
 
     let clickCountText = 'Начни добывать любое яйцо';
     updateClickCounter(clickCountText);
-    changeBackgroundByRarity(currentEgg.rarity);
+    changeBackgroundByRarity();
 
     switch (rarity) {
         case 'Common':
