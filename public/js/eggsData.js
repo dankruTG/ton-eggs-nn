6f3e0c768234253e732aaeb1f85c3b802590563f
dankruTG <dankrute@gmail.com>
2024-06-13 19:06:06 +0300

    2 new eggs

--- public/js/eggsData.js
+++ public/js/eggsData.js
@@ -1,7 +1,9 @@
 const eggs = [
     { name: "Svinstvo Egg", icon: "public/images/myficalEggs/SvinstvoEgg.png", rarity: "Myfical", strength: 10000, minting: "maybe" },
     { name: "Rubin Egg", icon: "public/images/rareEggs/RubinEgg.png", rarity: "Secret", strength: 2500, minting: "no" },
+    { name: "Topaz Egg", icon: "public/images/secretEggs/topazEgg.png", rarity: "Secret", strength: 2500, minting: "no" },
     { name: "Golden Egg", icon: "public/images/uncommonEggs/goldenEgg.png", rarity: "Rare", strength: 1000, minting: "no" },
+    { name: "Opal Egg", icon: "public/images/rareEggs/opalEgg.png", rarity: "Rare", strength: 1000, minting: "no" },
     { name: "Silver Egg", icon: "public/images/uncommonEggs/silverEgg.png", rarity: "Uncommon", strength: 500, minting: "no" },
     { name: "Bronze Egg", icon: "public/images/commonEggs/bronzeEgg.png", rarity: "Common", strength: 100, minting: "no" }
     // Добавьте сколько угодно объектов с данными о яйцах
